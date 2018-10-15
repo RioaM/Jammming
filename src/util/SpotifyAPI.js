d9eac77d086669f7f8c326d58ca7389db0f4408d
Rioa Mattsson <rioamattsson@gmail.com>
2018-10-15 09:53:21 -0700

    Fixed previously mentioned bug

--- src/util/SpotifyAPI.js
+++ src/util/SpotifyAPI.js
@@ -9,21 +9,21 @@ export const SpotifyAPI = {
     {
       headers: {"Authorization": `Bearer ${token}`}
     }).then(response => {
-      return response.json();
-  }).then(jsonResponse => {
-      if(jsonResponse.tracks) {
-        return jsonResponse.tracks.items.map(track => {
-          const artists = track.artists.map(artist => {
-            return artist.name;
-          }).join(', ');
-          return {
-            trackName: track.name,
-            artists: artists,
-            album: track.album.name,
-            trackID: track.id,
-            trackURI: track.uri
-          };
-        });
+        return response.json();
+    }).then(jsonResponse => {
+        if(jsonResponse.tracks) {
+          return jsonResponse.tracks.items.map(track => {
+            const artists = track.artists.map(artist => {
+              return artist.name;
+            }).join(', ');
+            return {
+              trackName: track.name,
+              artists: artists,
+              album: track.album.name,
+              trackID: track.id,
+              trackURI: track.uri
+            };
+          });
       } else {
         const str = JSON.stringify(jsonResponse, null, 4); // (Optional) beautiful indented output.
         console.log('Response:' + str);
@@ -33,7 +33,7 @@ export const SpotifyAPI = {
   },
   savePlaylist: (playlistName, playlist) => {
     const uriArray = playlist.map(track => {
-      return `spotify:track:${track.trackURI}`;
+      return `${track.trackURI}`;
     })
     fetch('https://api.spotify.com/v1/me',
       {
@@ -46,7 +46,7 @@ export const SpotifyAPI = {
     }).then(jsonResponse => {
       return jsonResponse.id;
     }).then(user_id => {
-      fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
+      return fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${token}`,
@@ -56,8 +56,6 @@ export const SpotifyAPI = {
           "name": `${playlistName}`})
         }
     )}).then(response => {
-      console.log('server response :', response);
-      console.log('another', JSON.parse(response.body).data);
       return response.json();
     }).then(jsonResponse => {
         return jsonResponse.id;
@@ -69,7 +67,7 @@ export const SpotifyAPI = {
             "Authorization": `Bearer ${token}`,
             "Content-Type": "application/json; charset=utf-8"
           },
-          body: JSON.stringify({uriArray})
+          body: JSON.stringify({uris: uriArray})
       })
     });
   },
@@ -79,8 +77,6 @@ export const SpotifyAPI = {
     } else if(window.location.href.match(/access_token=([^&]*)/)){
       token = window.location.href.match(/access_token=([^&]*)/)[0].split('=')[1];
       let expiration = window.location.href.match(/expires_in=([^&]*)/)[0].split('=')[1];
-      //setTimeout(()=>{ token = ""}, expiration * 1000);
-      console.log('experation:' + expiration, token);
       window.setTimeout(() => token = '', expiration * 1000);
       window.history.pushState('Access Token', null, '/');
       return token;
